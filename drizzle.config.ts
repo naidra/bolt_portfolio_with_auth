f4de3f31b1908c4f1b94eff6695aaeec148c1c02
Ardian Sallauka <ardi_c_cc@hotmail.com>
2025-05-16 19:03:45 +0200

    Changed drizzle config added nested css

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,13 @@ import type { Config } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+// This is a configuration file for drizzle-kit, a TypeScript ORM for PostgreSQL.
+
 export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
-  driver: 'pg',
+  driver: 'pglite',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+} as Config;
