3b3d071cd85c3eda55670c26967fa2801db76e86
Ardian Sallauka <ardi_c_cc@hotmail.com>
2025-05-16 19:38:11 +0200

    Reverser some package versions as they were before

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ dotenv.config();
 export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
-  driver: 'pglite',
+  driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
